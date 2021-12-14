ff9d4b5765eae0dc0599b08920fcc02715488856
jaladis <sj235@njit.edu>
2021-12-14 09:06:59 -0500

    updated  files

--- create_fake_users.py
+++ create_fake_users.py
@@ -22,4 +22,4 @@ if __name__ == '__main__':
     if len(sys.argv) <= 1:
         print('Pass the number of users you want to create as an argument.')
         sys.exit(1)
-    create_fake_users(int(sys.argv[1]))
+    create_fake_users(int(sys.argv[10]))
